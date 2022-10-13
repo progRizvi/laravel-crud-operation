977c28f0a4bedd2d6d814b1f48e86c560db77a66
progRizvi <prog.rizvi@gmail.com>
2022-10-13 15:14:29 +0600

    Product Read from database

---
+++ resources/views/updateView.blade.php
@@ -0,0 +1,11 @@
+@extends('welcome')
+
+@section('update')
+    <div class="col-md-4 m-auto">
+        <form action="">
+            <div class="mb-2 ">
+                <label for="ProductName"></label>
+            </div>
+        </form>
+    </div>
+@endsection
